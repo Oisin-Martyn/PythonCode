c0c958f5e324c0a88c0deb5853fdb6b5e51aa6a3
Oisin-Martyn <oisinmartyn06@gmail.com>
2026-07-10 13:22:11 +0100

    Update hello.py 1h 7min

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 #Ask user for their name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
 
-#Remove whitespace from string and capitalize user's name
-name = name.strip().title()
+#Split user's name into first name and last name
+first, last = name.split(" ")
+
 
 #Say Hello to user
-print(f"hello, {name}")
+print(f"hello, {first}")
