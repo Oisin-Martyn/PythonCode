2b3263daac794ca0c10a35b81dd6d67bdcc76185
Oisin-Martyn <oisinmartyn06@gmail.com>
2026-07-12 15:02:43 +0100

    Update hello.py 1h 46min

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-#Ask user for their name
-name = input("What's your name? ").strip().title()
-
-#Split user's name into first name and last name
-first, last = name.split(" ")
+def main():
+    name = input("What's your name? ")
+    hello(name)
 
 
-#Say Hello to user
-print(f"hello, {first}")
+def hello(to="world"):
+    print("hello,", to)
+
+
+
+main()
